e80e694842a09f9f83e81757c7c95fb7d39d3fce
acm-sp85 <acontell@gmail.com>
2021-06-24 08:50:45 -0400

    bootstrap installed. form styled at 10%

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
+import Form from "react-bootstrap/Form";
 
-export default class Form extends Component {
+export default class FormSubmit extends Component {
   state = {
     name: "",
     brand: "",
@@ -10,7 +11,7 @@ export default class Form extends Component {
     amount: 0,
     purchasePrice: 0,
     marketPrice: 0,
-    img: "",
+    img_url: "",
     serialNumber: "",
     notes: "",
   };
@@ -45,111 +46,126 @@ export default class Form extends Component {
       amount,
       purchasePrice,
       marketPrice,
-      img,
+      img_url,
       serialNumber,
       notes,
     } = this.state;
     return (
-      <form onSubmit={this.handleSubmit} className="form">
-        <label>
-          Name:
-          <input
-            name="name"
-            type="text"
-            value={name}
-            onChange={this.handleChange}
-          />
-        </label>
-        <label>
-          Brand:
-          <input
-            name="brand"
-            type="text"
-            value={brand}
-            onChange={this.handleChange}
-          />
-        </label>
-        <label>
-          Item type:
-          <select name="type" value={type} onChange={this.handleChange}>
-            <option value=""></option>
-            <option value="camera">Camera</option>
-            <option value="lens">Lens</option>
-          </select>
-        </label>
-        <label>
-          Analog/Digital:
-          <select name="medium" value={medium} onChange={this.handleChange}>
-            <option value=""></option>
-            <option value="digital">Digital</option>
-            <option value="analog">Analog</option>
-          </select>
-        </label>
-        <label>
-          Category:
-          <select name="category" value={category} onChange={this.handleChange}>
-            <option value=""></option>
-            <option value="photo">Photo</option>
-            <option value="video">Video</option>
-          </select>
-        </label>
-        <label>
-          Amount:
-          <input
-            name="amount"
-            type="number"
-            value={amount}
-            onChange={this.handleChange}
-          />
-        </label>
-        <label>
-          Purchase Price:
-          <input
-            name="purchasePrice"
-            type="number"
-            value={purchasePrice}
-            onChange={this.handleChange}
-          />
-        </label>
-        <label>
-          Market Price:
-          <input
-            name="marketPrice"
-            type="number"
-            value={marketPrice}
-            onChange={this.handleChange}
-          />
-        </label>
-        <label>
-          Image link:
-          <input
-            name="img"
-            type="text"
-            value={img}
-            onChange={this.handleChange}
-          />
-        </label>
-        <label>
-          Serial Number:
-          <input
-            name="serialNumber"
-            type="text"
-            value={serialNumber}
-            onChange={this.handleChange}
-          />
-        </label>
-        <label>
-          Notes:
-          <input
-            name="notes"
-            type="text"
-            value={notes}
-            onChange={this.handleChange}
-          />
-        </label>
+      <Form onSubmit={this.handleSubmit} className="form">
+        <Form.Group>
+          <label>
+            Name:
+            <input
+              name="name"
+              type="text"
+              value={name}
+              onChange={this.handleChange}
+            />
+          </label>
+
+          <label>
+            Brand:
+            <input
+              name="brand"
+              type="text"
+              value={brand}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            Item type:
+            <select name="type" value={type} onChange={this.handleChange}>
+              <option value=""></option>
+              <option value="camera">Camera</option>
+              <option value="lens">Lens</option>
+            </select>
+          </label>
+        </Form.Group>
+        <Form.Group>
+          <label>
+            Analog/Digital:
+            <select name="medium" value={medium} onChange={this.handleChange}>
+              <option value=""></option>
+              <option value="digital">Digital</option>
+              <option value="analog">Analog</option>
+            </select>
+          </label>
+          <label>
+            Category:
+            <select
+              name="category"
+              value={category}
+              onChange={this.handleChange}
+            >
+              <option value=""></option>
+              <option value="photography">Photo</option>
+              <option value="video">Video</option>
+            </select>
+          </label>
+          <label>
+            Amount:
+            <input
+              name="amount"
+              type="number"
+              value={amount}
+              onChange={this.handleChange}
+            />
+          </label>
+        </Form.Group>
+        <Form.Group>
+          <label>
+            Purchase Price:
+            <input
+              name="purchasePrice"
+              type="number"
+              value={purchasePrice}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            Market Price:
+            <input
+              name="marketPrice"
+              type="number"
+              value={marketPrice}
+              onChange={this.handleChange}
+            />
+          </label>
+        </Form.Group>
+        <Form.Group>
+          <label>
+            Image link:
+            <input
+              name="img_url"
+              type="text"
+              value={img_url}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            Serial Number:
+            <input
+              name="serialNumber"
+              type="text"
+              value={serialNumber}
+              onChange={this.handleChange}
+            />
+          </label>
+        </Form.Group>
+        <Form.Group>
+          <label>
+            Notes:
+            <input
+              name="notes"
+              type="text"
+              value={notes}
+              onChange={this.handleChange}
+            />
+          </label>
+        </Form.Group>
 
         <input type="submit" value="Submit" />
-      </form>
+      </Form>
     );
   }
 }
